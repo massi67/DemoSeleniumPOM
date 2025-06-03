5c47781ad8ec6a879658ae6d2990b42c2c92f7ea
basavaraj.enagi <basavaraj.enagi@hcltech.com>
2025-06-03 12:43:56 +0530

    pushing test change by Basavaraj

--- src/test/java/Pages/SportsPage.java
+++ src/test/java/Pages/SportsPage.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SportsPage {
-	
+	//change by Basavaraj
 	WebDriver driver;
 	
 	@FindBy(xpath = "//h2")
